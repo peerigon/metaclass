8c5f957ae6fb95d661079745a0723b3bfc5aff0a
Johannes <mail@johannesewald.de>
2012-04-23 01:58:21 +0200

    Introduced getInheritedProperties

--- test/Class.class.test.js
+++ test/Class.class.test.js
@@ -4,9 +4,10 @@ var expect = require("expect.js"),
     is = require("../lib/helpers/is"),
     Class = require("../lib/Class.class"),
     AbstractProperty = require("../lib/AbstractProperty.class"),
-    Visibility = require("../lib/Visibility.class"),
+    PropertyFilter = require("../lib/helpers/PropertyFilter.class"),
     Method = require("../lib/Method.class"),
     Property = require("../lib/Property.class"),
+    Visibility = require("../lib/Visibility.class"),
     combineStrings = require("./testHelpers/combineStrings.js"),
     createProperties = require("./testHelpers/createProperties.js"),
     sortByPropertyName = require("./testHelpers/sortByPropertyName.js"),
@@ -32,6 +33,41 @@ describe("Class", function () {
     });
     it("should return true", function () {
         expect(is(instance).instanceOf(Class)).to.be(true);
+    });
+    describe("#setPath", function () {
+        it("should return the instance", function () {
+            expect(instance.setPath(null)).to.be(instance);
+            expect(instance.setPath(__filename)).to.be(instance);
+        });
+        it("should throw an exception", function () {
+            expect(function () {
+                instance.setPath(undefined);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setPath(true);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setPath(2);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setPath([]);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setPath({});
+            }).to.throwException(checkForTypeError);
+        });
+    });
+    describe("#getPath", function () {
+        it("should return null", function () {
+            expect(instance.getPath()).to.be(null);
+            instance.setPath(__dirname);
+            instance.setPath(null);
+            expect(instance.getPath()).to.be(null);
+        });
+        it("should return __dirname", function () {
+            instance.setPath(__dirname);
+            expect(instance.getPath()).to.be(__dirname);
+        });
     });
     describe("#setClassId", function () {
         it("should return the instance", function () {
@@ -90,39 +126,80 @@ describe("Class", function () {
             expect(instance.getClassName()).to.be("TestClass");
         });
     });
-    describe("#setPath", function () {
+    describe("#setSuperClass", function () {
         it("should return the instance", function () {
-            expect(instance.setPath(null)).to.be(instance);
-            expect(instance.setPath(__filename)).to.be(instance);
+            var superClass = new Class();
+
+            expect(instance.setSuperClass(superClass)).to.be(instance);
         });
         it("should throw an exception", function () {
             expect(function () {
-                instance.setPath(undefined);
+                instance.setSuperClass(undefined);
             }).to.throwException(checkForTypeError);
             expect(function () {
-                instance.setPath(true);
+                instance.setSuperClass(true);
             }).to.throwException(checkForTypeError);
             expect(function () {
-                instance.setPath(2);
+                instance.setSuperClass(2);
             }).to.throwException(checkForTypeError);
             expect(function () {
-                instance.setPath([]);
+                instance.setSuperClass("someString");
             }).to.throwException(checkForTypeError);
             expect(function () {
-                instance.setPath({});
+                instance.setSuperClass({});
             }).to.throwException(checkForTypeError);
         });
     });
-    describe("#getPath", function () {
+    describe("#getSuperClass", function () {
         it("should return null", function () {
-            expect(instance.getPath()).to.be(null);
-            instance.setPath(__dirname);
-            instance.setPath(null);
-            expect(instance.getPath()).to.be(null);
+            var superClass = new Class();
+
+            expect(instance.getSuperClass()).to.be(null);
+            instance.setSuperClass(superClass);
+            instance.setSuperClass(null);
+            expect(instance.getSuperClass()).to.be(null);
         });
-        it("should return __dirname", function () {
-            instance.setPath(__dirname);
-            expect(instance.getPath()).to.be(__dirname);
+        it("should return the superClass", function () {
+            var superClass = new Class();
+
+            instance.setSuperClass(superClass);
+            expect(instance.getSuperClass()).to.be(superClass);
+        });
+    });
+    describe("#setConstructor", function () {
+        it("should return the instance", function () {
+            expect(instance.setConstructor(function () {})).to.be(instance);
+        });
+        it("should throw an exception", function () {
+            expect(function () {
+                instance.setConstructor(undefined);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setConstructor(true);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setConstructor(2);
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setConstructor("someString");
+            }).to.throwException(checkForTypeError);
+            expect(function () {
+                instance.setConstructor({});
+            }).to.throwException(checkForTypeError);
+        });
+    });
+    describe("#getConstructor", function () {
+        it("should return null", function () {
+            expect(instance.getConstructor()).to.be(null);
+            instance.setConstructor(function () {});
+            instance.setConstructor(null);
+            expect(instance.getConstructor()).to.be(null);
+        });
+        it("should return the constructor", function () {
+            function constructor() {}
+
+            instance.setConstructor(constructor);
+            expect(instance.getConstructor()).to.be(constructor);
         });
     });
     describe("#addProperty", function () {
@@ -239,48 +316,6 @@ describe("Class", function () {
             }).to.throwException(checkForTypeError);
         });
     });
-    describe("#setSuperClass", function () {
-        it("should return the instance", function () {
-            var superClass = new Class();
-
-            expect(instance.setSuperClass(superClass)).to.be(instance);
-        });
-        it("should throw an exception", function () {
-            var exampleClass = new Class();
-
-            expect(function () {
-                instance.setSuperClass(undefined);
-            }).to.throwException(checkForTypeError);
-            expect(function () {
-                instance.setSuperClass(true);
-            }).to.throwException(checkForTypeError);
-            expect(function () {
-                instance.setSuperClass(2);
-            }).to.throwException(checkForTypeError);
-            expect(function () {
-                instance.setSuperClass("someString");
-            }).to.throwException(checkForTypeError);
-            expect(function () {
-                instance.setSuperClass({});
-            }).to.throwException(checkForTypeError);
-        });
-    });
-    describe("#getSuperClass", function () {
-        it("should return null", function () {
-            var superClass = new Class();
-
-            expect(instance.getSuperClass()).to.be(null);
-            instance.setSuperClass(superClass);
-            instance.setSuperClass(null);
-            expect(instance.getSuperClass()).to.be(null);
-        });
-        it("should return the superClass", function () {
-            var superClass = new Class();
-
-            instance.setSuperClass(superClass);
-            expect(instance.getSuperClass()).to.be(superClass);
-        });
-    });
     describe("#getProperties", function () {
         it("should return an empty array on a new class", function () {
             expect(instance.getProperties()).to.eql([]);
@@ -298,6 +333,104 @@ describe("Class", function () {
             result = instance.getProperties();
             result.sort(sortByPropertyName);
             expect(result).to.eql(allPossibleProperties);
+        });
+        it("should exclude inherited properties", function () {
+            var superClass = new Class(),
+                someProperty = new Property();
+
+            someProperty.setName("someProperty");
+            superClass.addProperty(someProperty);
+            instance.setSuperClass(superClass);
+            expect(instance.getProperties()).to.have.length(0);
+        });
+    });
+    describe("#getInheritedProperties", function () {
+        it("should return an empty array on a class with no super class", function () {
+            var someProperty = new Property();
+
+            someProperty.setName("someProperty");
+            instance.addProperty(someProperty);
+            expect(instance.getInheritedProperties()).to.have.length(0);
+        });
+        it("should return all non-private properties from the super class", function () {
+            var superClass = new Class(),
+                i,
+                result,
+                currentProperty,
+                allPossiblePropertiesWithoutPrivate;
+
+            allPossiblePropertiesWithoutPrivate = allPossibleProperties.filter(function (property) {
+                return property.getVisibility() !== Visibility.PRIVATE;
+            });
+
+            for (i = 0; i < allPossibleProperties.length; i++) {
+                currentProperty = allPossibleProperties[i];
+                superClass.addProperty(currentProperty);
+            }
+
+            instance.setSuperClass(superClass);
+            result = instance.getInheritedProperties();
+            result.sort(sortByPropertyName);
+            expect(result).to.eql(allPossiblePropertiesWithoutPrivate);
+        });
+        it("should work with super super classes as well", function () {
+            var superClass = new Class(),
+                superSuperClass = new Class(),
+                i,
+                result,
+                currentProperty,
+                allPossiblePropertiesWithoutPrivate;
+
+            allPossiblePropertiesWithoutPrivate = allPossibleProperties.filter(function (property) {
+                return property.getVisibility() !== Visibility.PRIVATE;
+            });
+
+            for (i = 0; i < allPossibleProperties.length; i++) {
+                currentProperty = allPossibleProperties[i];
+                superSuperClass.addProperty(currentProperty);
+            }
+
+            superClass.setSuperClass(superSuperClass);
+            instance.setSuperClass(superClass);
+            result = instance.getInheritedProperties();
+            result.sort(sortByPropertyName);
+            expect(result).to.eql(allPossiblePropertiesWithoutPrivate);
+        });
+        it("should return only non-overridden properties", function () {
+            var originalProperty = new Property(),
+                overridingProperty = new Property(),
+                originalMethod = new Method(),
+                overridingMethod = new Method(),
+                superClass = new Class(),
+                superSuperClass = new Class(),
+                result;
+
+            originalProperty
+                .setName("property")
+                .setVisibility(Visibility.PUBLIC);
+            overridingProperty
+                .setName("property")
+                .setVisibility(Visibility.PUBLIC);
+
+            originalMethod
+                .setName("method")
+                .setVisibility(Visibility.PUBLIC);
+            overridingMethod
+                .setName("method")
+                .setVisibility(Visibility.PUBLIC);
+
+            superSuperClass
+                .addProperty(originalProperty)
+                .addProperty(originalMethod);
+            superClass
+                .addProperty(overridingProperty)
+                .addProperty(overridingMethod);
+
+            superClass.setSuperClass(superSuperClass);
+            instance.setSuperClass(superClass);
+            result = instance.getInheritedProperties();
+            result.sort(sortByPropertyName);
+            expect(result).to.eql([overridingMethod, overridingProperty]);
         });
     });
 });

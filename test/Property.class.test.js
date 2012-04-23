be7c9755aefe3528624a2cc3f4b5d93df513789f
Johannes <mail@johannesewald.de>
2012-04-23 12:30:09 +0200

    setInitialValue now takes JavaScript-values instead of eval-ready strings

--- test/Property.class.test.js
+++ test/Property.class.test.js
@@ -21,29 +21,17 @@ describe("Property", function () {
             expect(instance.setInitialValue("Hello")).to.be(instance);
             expect(instance.setInitialValue(null)).to.be(instance);
         });
-        it("should throw an exception", function () {
-            expect(function () {
-                instance.setInitialValue(undefined);
-            }).to.throwException();
-            expect(function () {
-                instance.setInitialValue(true);
-            }).to.throwException();
-            expect(function () {
-                instance.setInitialValue(1);
-            }).to.throwException();
-            expect(function () {
-                instance.setInitialValue({});
-            }).to.throwException();
-        });
     });
     describe("#getInitialValue", function () {
         it("should return the '[1, 2, 3]'", function () {
-            instance.setInitialValue("[1, 2, 3]");
-            expect(instance.getInitialValue()).to.be("[1, 2, 3]");
+            var arr = [1, 2, 3];
+
+            instance.setInitialValue(arr);
+            expect(instance.getInitialValue()).to.be(arr);
         });
         it("should return null", function () {
             expect(instance.getInitialValue()).to.be(null);
-            instance.setInitialValue("[1, 2, 3]");
+            instance.setInitialValue([1, 2, 3]);
             instance.setInitialValue(null);
             expect(instance.getInitialValue()).to.be(null);
         });
